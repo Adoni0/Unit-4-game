ddb1e3261275c4c422f8aa036c7d51f0e4afcf73
Adam SOudani <asoudani@email.com>
2020-03-04 15:39:59 -0800

    Initial site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,63 +5,83 @@ var wins = 0;
 var losses = 0;
 var numberOptions = [12, 5, 2, 9];//scores will be assigned to crystals at random and switched after every game
 
+var random = Math.floor(Math.random() * 4);
+var crystal1 = randomInterval(1, 12);
+console.log(crystal1);
+var crystal2 = randomInterval(1, 12);
+var crystal3 = randomInterval(1, 12);
+var crystal4 = randomInterval(1, 12);
 
-function initializeGame(){
+
+    //initialize game again once scoreCounter=targetNumber or scoreCounter>=targetNumber
+function randomInterval(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+
+
+function startGame(){
     scoreCounter = 0;
     wins = 0;
     losses = 0;
     targetNumber = Math.floor((Math.random() * 120)+ 19);
     console.log(targetNumber)
     $("#number-to-guess").text(targetNumber);
+    $('#crystal1').on('click', function(){
+        console.log('crystal1 clicked')
+        this.value = crystal1;
+        $('#total-score').text(scoreCounter += this.value);
+    })
+        
+        $('#crystal2').on('click', function(){
+            console.log('crystal1 clicked')
+            this.value = crystal2;
+            $('#total-score').text(scoreCounter += this.value);
+        })
+
+            $('#crystal3').on('click', function(){
+                console.log('crystal1 clicked')
+                this.value = crystal3;
+                $('#total-score').text(scoreCounter += this.value);
+            })
+
+                $('#crystal4').on('click', function(){
+                    console.log('crystal1 clicked')
+                    this.value = crystal4;
+                    $('#total-score').text(scoreCounter += this.value);
+                })
+
+                $('.crystal').on('click', function(){
+                    if(scoreCounter === targetNumber){
+                        $('#result-text').text('You WIN!');
+                        $('#wins').text('Wins: '+ wins++);
+                        resetGame();
+                    }else if(scoreCounter > targetNumber){
+                        $('#result-text').text('You LOSE!');
+                        $('#losses').text(`Losses: ${losses++}`);
+                        resetGame();
+                    }
+                })
+    }
+
+function resetGame(){
+    crystal1 = randomInterval(1, 12);
+    crystal2 = randomInterval(1, 12);
+    crystal3 = randomInterval(1, 12);
+    crystal4 = randomInterval(1, 12);
+    scoreCounter = 0;
+    targetNumber = Math.floor((Math.random() * 120)+ 19);
+    $('#number-to-guess').text(targetNumber);
+    $('#total-score').text(scoreCounter);
 }
-
-    //initialize game again once scoreCounter=targetNumber or scoreCounter>=targetNumber
-
-
-for(var i=0; i < numberOptions.length; i++){
     
-    var imageCrystalOne = $("<img>");
 
-    var imageCrystalTwo = $('<img>');
 
-    var imageCrystalThree = $("<img>");
+startGame();
 
-    var imageCrystalFour = $('<img>');
 
-    imageCrystalOne, imageCrystalTwo, imageCrystalThree, imageCrystalFour.addClass('crystal-image');
 
-    imageCrystalOne.attr('src', "https://cdn.pixabay.com/photo/2014/04/02/16/26/crystal-307265_960_720.png");
 
-    imageCrystalTwo.attr('src', "https://www.pinclipart.com/picdir/middle/187-1878291_free-vector-graphics-on-pixabay-image-freeuse-download.png");
-
-    imageCrystalThree.attr('src', 'https://webstockreview.net/images/crystal-clipart-cartoon-3.png');
-
-    imageCrystalFour.attr('src', 'https://clipartmag.com/images/crystal-clipart-21.png');
-
-    imageCrystalOne, imageCrystalTwo, imageCrystalThree, imageCrystalFour.attr('data-crystalvalue', numberOptions[i]);
-
-    $('#crystals').append(imageCrystalOne, imageCrystalTwo, imageCrystalThree, imageCrystalFour);
-
-}
-
-$(".crystal-image").on("click", function(){
-    var crystalvalue = ($(this).attr('data-crystalvalue'));
-    crystalvalue = parseInt(crystalvalue);
-
-    scoreCounter += crystalvalue;
-    $('#total-score').text(scoreCounter); //have total score displayed, which is equal to the scoreCounter
-
-    if (scoreCounter === targetNumber){
-        wins++;
-        $('#wins').text(wins);
-        initializeGame();
-    }
-
-    else if (scoreCounter >= targetNumber){
-        losses++;
-        $('#losses').text(losses);
-        initializeGame();
-    }
-});
 
 //have values for crystals change after game is over (when scoreCounter === targetNumber || scoreCounter >= targetNumber)
